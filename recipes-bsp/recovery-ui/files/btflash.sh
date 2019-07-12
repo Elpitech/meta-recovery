a530c871126ecb571b37bec818c012e557f9c52d
Vadim V. Vlasov <vadim.vlasov@t-platforms.ru>
2019-07-12 16:32:14 +0300

    distro: Create .dialogrc with red color scheme
    This restores original red background when confirmation
    is requested for ROM rewrite.
    
    Signed-off-by: Vadim V. Vlasov <vadim.vlasov@t-platforms.ru>

--- recipes-bsp/recovery-ui/files/btflash.sh
+++ recipes-bsp/recovery-ui/files/btflash.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 
 ROM=$1
+if [ -x /usr/bin/dialog ]; then
+    DIALOGRC=${HOME}/.dialogrc
+    dialog --create-rc ${DIALOGRC}
+    sed -i -e 's/screen_color.*$/screen_color = (BLACK,RED,ON)/
+s/use_colors.*$/use_colors = ON/' ${DIALOGRC}
+    export DIALOGRC
+fi
+
 clear
 cd ${VAR_PREFIX}
-HOME=/etc/dialog/red dialog --colors --no-shadow --defaultno --backtitle '\Zb\Z0mITX ROM update\Zn' --yesno "\Zb\Z0DANGER!\nRewrite ROM ?\Zn" 12 40
+dialog --colors --no-shadow --defaultno --backtitle '\Zb\Z0mITX ROM update\Zn' --yesno "\Zb\Z0DANGER!\nRewrite ROM ?\Zn" 12 40
 ret=$?
 if [ ! ${ret} -eq 0 ]; then
     clear
     exit 0
 fi
 clear
-HOME=/etc/dialog/red dialog --colors --no-shadow --backtitle '\Zb\Z0mITX ROM update confirmation\Zn' --yesno "\Zb\Z0Cancel ROM rewrite?\Zn" 12 40
+dialog --colors --no-shadow --backtitle '\Zb\Z0mITX ROM update confirmation\Zn' --yesno "\Zb\Z0Cancel ROM rewrite?\Zn" 12 40
 ret=$?
 if [ ${ret} -eq 0 ]; then
     clear
